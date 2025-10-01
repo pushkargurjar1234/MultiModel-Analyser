2d03c52c0f5793a4d85bdc22a551c4abd9ae0137
PUSHKAR SINGH GURJAR <pushkargurjar1234@gmail.com>
2025-10-01 10:49:52 +0530

    The reduced size of model

--- backend/models.py
+++ backend/models.py
@@ -2,15 +2,28 @@
 from transformers import pipeline  #type: ignore
 from ultralytics import YOLO
 import os
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 def load_models():
     """Load all ml  models and return them as dictionary"""
     print("Loading models... This might take a momentt.")
 
     # NLP Models
-    sentiment_analyzer = pipeline("sentiment-analysis",
-                        model="cardiffnlp/twitter-roberta-base-sentiment")          #type: ignore
-    
+    # sentiment_analyzer = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")          #type: ignore
+   
+    model_name = "cardiffnlp/twitter-roberta-base-sentiment"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(
+    model_name,
+    device_map="auto",
+    load_in_8bit=True   # <- quantized 
+    )
+    sentiment_analyzer = pipeline(
+    "sentiment-analysis",
+    model=model,
+    tokenizer=tokenizer)
+
+
     summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-6-6")
     
     topic_classifier = pipeline("zero-shot-classification",model="facebook/bart-large-mnli")
